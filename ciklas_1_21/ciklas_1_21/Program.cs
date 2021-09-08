e53c390a23c64f06d36b786618e3558c75ca1061
ZydrunasZujus <89936656+zydrunaszujus@users.noreply.github.com>
2021-09-08 23:18:20 +0300

    Ciklas 1-21

---
+++ ciklas_1_21/ciklas_1_21/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ciklas_1_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 21; i++)
+            {
+                int a = 5;
+                int b = a + i;
+                Console.WriteLine(a*b);
+            }
+        }
+    }
+}
