a47e65ba98913a1d1e8a95a249453ca79466c745
Virginijus1 <89936669+Virginijus1@users.noreply.github.com>
2021-09-08 21:07:20 +0300

    BP 17

--- Mano_darbas_darbas/Program.cs
+++ Mano_darbas_darbas/Program.cs
@@ -7,6 +7,7 @@ namespace Mano_darbas_darbas
         static void Main(string[] args)
         {
             Console.WriteLine("Iveskite pirma skaiciu");
+		int slyva = 1;
             int x=int.Parse(Console.ReadLine());
             Console.WriteLine("Iveskite antra skaiciu");
             int y = int.Parse(Console.ReadLine());
