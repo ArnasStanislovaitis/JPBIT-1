13d8493ff57f579d3ba41c7f85ababd082db8d7b
ZydrunasZujus <89936656+zydrunaszujus@users.noreply.github.com>
2021-09-08 22:31:30 +0300

    be e raides

--- BP-12/Program.cs
+++ BP-12/Program.cs
@@ -12,7 +12,7 @@ namespace Fraziu_spausdintuvas
 
         static void FraziuSpausdintuvas()
         {
-            Console.WriteLine("Iveskite fraze");
+            Console.WriteLine("Iveskite fraz");
             string Fraze = Console.ReadLine();
             Console.WriteLine("Iveskite kiek kartu norite isvesti fraze i ekrana");
             int Kartai = int.Parse(Console.ReadLine());
