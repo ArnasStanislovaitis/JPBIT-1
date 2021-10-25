1b8362841ce65c4ebe89157759a29e9a32201229
zydrunaszujus <zydriuszu@gmail.com>
2021-10-25 11:54:38 +0300

    Revert "Pataisytas žinutės langas"
    This reverts commit 6c103ad092b5d23e86ba964ddc7fe33956bacb0a.

--- projektas/src/app/komponentai/zinute/zinute.component.ts
+++ projektas/src/app/komponentai/zinute/zinute.component.ts
@@ -32,7 +32,6 @@ export class ZinuteComponent implements OnInit {
 
   getMessages() {
     this.api.getMessages().subscribe(data => this.api.messages = data, error => console.log(error));
-  
   }
 
   onSubmit(e: any) {
