1e7ecfa0e8d6c640e5db476232638cd4ae4e10fb
Egidijus Adzgauskas <eadzgauskas@gmail.com>
2021-10-29 20:07:39 +0300

    duomenu perdavimas is nerijaus db i console log
    pirmas zingsnis iki pilno perdavimo i FB

--- projektas/src/app/puslapiai/pagrindinis/pagrindinis.component.ts
+++ projektas/src/app/puslapiai/pagrindinis/pagrindinis.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/servisai/api.service';
+
 
 @Component({
   selector: 'app-pagrindinis',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagrindinisComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiServisas: ApiService) {
+    this.apiServisas.getMessages().subscribe((duomenys:any)=>{ 
+      console.log(duomenys);
+      this.NerijausZinutes=duomenys;
+      console.log(this.NerijausZinutes);
+
+     });
+   }
 
   ngOnInit(): void {
+
   }
 
+  NerijausZinutes= [];
+  
+
+  import(){
+    let zinutes = this.apiServisas.getMessages();
+
+  }
 }
